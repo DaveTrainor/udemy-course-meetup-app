d856ac5fd39b430d1cac1cabd28c357559d8c377
DaveTrainor <davetrainor79@gmail.com>
2023-03-06 16:50:34 +0000

    lesson 38

---
+++ src/tests/AllMeetups.test.js
@@ -0,0 +1,11 @@
+import App from "../App"
+import {render} from '@testing-library/react'
+import AllMeetupsPage from "../pages/AllMeetups"
+import React from "react";
+
+
+
+
+test ('returns 200 reponse when page is requested', ()=>{
+    expect (1+1).toEqual(2);
+});
